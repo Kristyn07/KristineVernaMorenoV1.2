1007a698a3021bb2b9f356eb042b12a03945a806
Kristyn07 <kristinevernamoreno1@gmail.com>
2024-11-06 06:25:54 +0800

    Bootstrap
    install using nugets bootstrap 5.3.3

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace KristineVernaMorenoV1._2
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews(); 
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
